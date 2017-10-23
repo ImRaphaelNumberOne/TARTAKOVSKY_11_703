1114f782232059f6dd6c1314cbab5ac0e66b878c
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-23 20:43:31 +0300

    Update THIRTYTWOb.java

--- HomeWork15102017/src/ru/itis/THIRTYTWOb.java
+++ HomeWork15102017/src/ru/itis/THIRTYTWOb.java
@@ -1,4 +1,4 @@
-package ru.itis;
+package ru.itis;//-
 
 import java.util.Scanner;
 
@@ -7,7 +7,7 @@ public class THIRTYTWOb {
         Scanner in = new Scanner(System.in);
         double  f = 0, x1 = 1, x0, eps = 0.0001;
         x0 = in.nextDouble();
-        if (Math.abs(x1 - x0) > eps) {
+        if (Math.abs(x1 - x0) > eps) {   //о чем речь?
             f = 1 + 9 * Math.sin(x0) / 2;
         }
         System.out.println(f);

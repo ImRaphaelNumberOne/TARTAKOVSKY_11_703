19be3f2db43d3a5f11df038d0276a547e05bec2f
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-23 20:51:19 +0300

    Update TWENTYFIVE.java

--- ClassWork11102017/src/ru/itis/TWENTYFIVE.java
+++ ClassWork11102017/src/ru/itis/TWENTYFIVE.java
@@ -1,4 +1,4 @@
-package ru.itis;
+package ru.itis;//+
 
 import java.util.Scanner;
 
@@ -16,7 +16,7 @@ public class TWENTYFIVE {
         }
         y1 = y0 - (y0 - x / (y0 * y0)) / 3;
             while (Math.abs(y1 - y0) > eps) {
-                y0=y1;
+                y0=y1;  // вот здесь же вы делаете перекидку. Почему она в домашках исчезает?
                 y1 = y0 - (y0 - x / (y0 * y0)) / 3;
             }
         System.out.println(y1);

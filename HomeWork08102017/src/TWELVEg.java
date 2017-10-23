bc5af6b197fa4c1b81629f59a6aadf9454c19584
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-23 20:35:06 +0300

    Update TWELVEg.java

--- HomeWork08102017/src/TWELVEg.java
+++ HomeWork08102017/src/TWELVEg.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner;//-
 
 public class TWELVEg {
     public static void main(String[] args) {
@@ -20,4 +20,4 @@ public class TWELVEg {
             System.out.println(S);
         }
     }
-}
+}// такие же замечания
